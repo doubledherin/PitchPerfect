3dac4312949dd006711a66c98aa0048ffcb65a1d
Wendy Dherin <wendydherin@gmail.com>
2018-12-23 14:01:27 +0100

    Initial commit.

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Wendy Dherin on 12/19/18.
+//  Copyright © 2018 WendyDherin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(<#T##animated: Bool##Bool#>)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button pressed.")
+        recordingLabel.text = "Recording in progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop recording button was pressed.")
+    }
+}
+
