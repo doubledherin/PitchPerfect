ef93126440d80217dd7086b199636622344feac9
Wendy Dherin <wendydherin@gmail.com>
2018-12-29 23:27:37 +0100

    Cast new ViewController as PlaySoundsViewController. Prepare PlaySoundsViewController for segue by indicating the url of the recorded audio.

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Wendy Dherin on 12/29/18.
+//  Copyright © 2018 WendyDherin. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    var recordedAudioUrl: URL!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
